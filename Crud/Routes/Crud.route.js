1c276ab7523c046a238f5c42661895154fcf64bd
Mohammad Saif <saifsami321md@gmail.com>
2022-12-13 11:32:53 +0530

    Error resolved, crud operation with get method done

--- Crud/Routes/Crud.route.js
+++ Crud/Routes/Crud.route.js
@@ -4,14 +4,19 @@ const CrudModel = require("../Model/Crud.model")
 
 const CrudRouter = express.Router();
 
-CrudRouter.get("/",(req,res)=>{
-    const user = CrudModel.find()
-    res.send(user)
+CrudRouter.get("/",async(req,res)=>{
+   try{
+         const user = await CrudModel.find()
+         res.status(200).send(user);
+   }
+   catch(err){
+         res.status(500).send({message:err.message})
+   }
 })
 
-CrudRouter.post("/post",(req,res)=>{
-    const user = CrudModel.create(req.body)
-    res.send({message:"user details saved"})
-})
+// CrudRouter.post("/post",(req,res)=>{
+//     const user = CrudModel.create(req.body)
+//     res.send({message:"user details saved"})
+// })
 
 module.exports = CrudRouter
--- Crud/index.js
+++ Crud/index.js
@@ -1,21 +1,31 @@
-const express = require("express")
-require("dotenv").config()
-const cors = require("cors")
-
-
-const connection = require("./config")
-const CrudRoute = require("./Routes/Crud.route")
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.use("/crud",CrudRoute)
+const connection = require("./config")
+const CrudRoute = require("./Routes/Crud.route")
+
+app.get("/",(req,res)=>{
+    res.send("Welcome from Crud Operation")
+})
+
+app.use("/nem",CrudRoute)
 
 const PORT  = process.env.PORT || 8080;
 
 
 
-app.listen(8080,()=>{
-    console.log("Listening on port localhost 8080")
+app.listen(PORT,async()=>{
+    try{
+         await connection;
+         console.log("db connected");
+    }
+    catch(err){
+         console.log("Check error",err);
+    }
+    console.log(`listening on port ${PORT}`);
 })
