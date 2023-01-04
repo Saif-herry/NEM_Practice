d1a7c03d179b98a25451563f76a3045db10b753a
Mohammad Saif <saifsami321md@gmail.com>
2023-01-05 00:00:17 +0530

    crud pratice

--- Crud/index.js
+++ Crud/index.js
@@ -3,16 +3,13 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('Welcome in Home Page')
+    res.send('Welcome in crud')
 })
 
-app.get('/about',(req,res)=>{
-    res.send('Welcome in about page')
+app.get('/dashboard',(req,res)=>{
+    res.send('welcome in dashboard')
 })
 
 app.listen(8080,()=>{
     console.log(`listening on the port 8080`)
 })
-
-
-
