7aca7fbf73d7a49a52d3517173eb3f284884da59
Mohammad Saif <saifsami321md@gmail.com>
2023-01-07 12:37:40 +0530

    middleware practice

--- Crud/index.js
+++ Crud/index.js
@@ -2,7 +2,14 @@ const express = require('express')
 
 const app = express()
 
+app.use((req,res,next)=>{
+    console.log('Hellow middleware')
+    next()
+    console.log('Middleware again')
+})
+
 app.get('/',(req,res)=>{
+    console.log('welcome in Test')
     res.send('Welcome in Test')
 })
 
