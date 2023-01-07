fdd23b2b065c9dde7e9701e6fc7aa298892c1488
Mohammad Saif <saifsami321md@gmail.com>
2023-01-07 12:31:54 +0530

    server run on 8080 port

--- Crud/index.js
+++ Crud/index.js
@@ -3,13 +3,13 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('Welcome in crud')
+    res.send('Welcome in Test')
 })
 
-app.get('/dashboard',(req,res)=>{
-    res.send('welcome in dashboard')
+app.get('/dashboard',(req,res,next)=>{
+    res.send('Welcome in dashboard!')
 })
 
 app.listen(8080,()=>{
-    console.log(`listening on the port 8080`)
+    console.log('listening on the port 8008')
 })
