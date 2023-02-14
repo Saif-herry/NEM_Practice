c5ff9f994f69dc3d65982a74fb0ede1b1ea8465d
Mohammad Saif <saifsami321md@gmail.com>
2023-02-14 23:27:43 +0530

    update

--- Crud2/Model/crud.model.js
+++ Crud2/Model/crud.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const CrudSchema = new mongoose.Schema({
     "name":String,
     "age":Number,
-    "City":String
+    
 })
 
 const crudModel = mongoose.model('crud2',CrudSchema)
