ceb3e4e7093b3fdd1f386b440b947b5427dd4151
Mohammad Saif <saifsami321md@gmail.com>
2023-02-19 23:42:24 +0530

    post method

--- Crud2/Routes/Crud.routes.js
+++ Crud2/Routes/Crud.routes.js
@@ -14,4 +14,15 @@ CrudRouter.get('/',async(req,res)=>{
     }
 })
 
+CrudRouter.post("/post",async(req,res)=>{
+  try{
+const user = await crudModel.create(req.body)
+user.save()
+res.status(200).send(user)
+  }
+  catch(err){
+res.status(500).send({message:err.message})
+  }
+})
+
 module.exports = CrudRouter
