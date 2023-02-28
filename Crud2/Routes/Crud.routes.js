6e65806b9f0cc6c8d8a94a5ab11e54eb6bc54381
Mohammad Saif <saifsami321md@gmail.com>
2023-02-28 23:30:20 +0530

    update

--- Crud2/Routes/Crud.routes.js
+++ Crud2/Routes/Crud.routes.js
@@ -4,35 +4,33 @@ const crudModel = require('../Model/crud.model')
 
 const CrudRouter = express.Router()
 
-CrudRouter.get('/',async(req,res)=>{
-    try{
-      const user =await crudModel.find()
-      res.status(200).send(user)
-    }
-    catch(err){
-        res.status(500).send(err)
-    }
-})
-
-CrudRouter.post("/post",async(req,res)=>{
-  try{
-const user = await crudModel.create(req.body)
-user.save()
-res.status(200).send(user)
-  }
-  catch(err){
-res.status(500).send({message:err.message})
+CrudRouter.get('/', async (req, res) => {
+  try {
+    const user = await crudModel.find()
+    res.status(200).send(user)
+  } catch (err) {
+    res.status(500).send(err)
   }
 })
 
-CrudRouter.patch("/edit/id",async(req,res)=>{
-  try{
-const user = crudModel.findByIdAndUpdate(req.body,req.params.id)
-user.save()
-res.status(200).send(user)
+
+
+CrudRouter.delete('/delete/id', async (req, res) => {
+  try {
+    const user = crudModel.findByIdAndDelete({ _id: req.params.id })
+    res.status(200).send(user)
+  } catch (err) {
+    res.status(500).send({ message: err.message })
   }
-  catch(err){
-    res.status(500).send({message:err.message})
+})
+
+CrudRouter.patch('/edit/id', async (req, res) => {
+  try {
+    const user = crudModel.findByIdAndUpdate(req.body, req.params.id)
+    user.save()
+    res.status(200).send(user)
+  } catch (err) {
+    res.status(500).send({ message: err.message })
   }
 })
 
