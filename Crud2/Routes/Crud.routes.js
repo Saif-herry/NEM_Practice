93bf85b343c8a1cbb32b4dfb41a21e02963cb070
Mohammad Saif <saifsami321md@gmail.com>
2023-03-04 23:43:34 +0530

    update

--- Crud2/Routes/Crud.routes.js
+++ Crud2/Routes/Crud.routes.js
@@ -4,6 +4,14 @@ const crudModel = require('../Model/crud.model')
 
 const CrudRouter = express.Router()
 
+CrudRouter.get('/', async (req, res) => {
+  try {
+    const user = await crudModel.find()
+    res.status(200).send(user)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
 
 CrudRouter.post('/post', async (req, res) => {
   try {
